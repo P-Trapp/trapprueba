b25280bff3288dd82a2de5268ab62d7180270943
P-Trapp <galexisf85@gmail.com>
2025-05-09 20:22:48 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function verMasEnHome(){
+    let mostrarCont = document.getElementById("verMas");
+    mostrarCont.style.display = "block";
+
+    let noFlex = document.getElementById("notasDev");
+    noFlex.style.display = "block";
+    noFlex.style.justifyContent = "center";
+}
